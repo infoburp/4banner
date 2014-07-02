8ac68a1fc28000cb4a48bfa744d3657defc9f595
Graeme Wolfendale <gwolfendale@gmail.com>
2014-07-02 21:01:09 +0100

    error handling for 404 etc

--- 4banner34.py
+++ 4banner34.py
@@ -2,12 +2,20 @@ import urllib.request, urllib.parse, urllib.error, os
 for x in range(0, 400):
         url="https://s.4cdn.org/image/title/"+str(x)+".gif"
         print(url)
-        urllib.request.urlretrieve(url,str(x)+".gif")
+        try:
+                urllib.request.urlretrieve(url,str(x)+".gif")
+        except urllib.error.HTTPError as err:
+                print(err.code)
         url="https://s.4cdn.org/image/title/"+str(x)+".jpg"
         print(url)
-        urllib.request.urlretrieve(url,str(x)+".jpg")
+        try:
+                urllib.request.urlretrieve(url,str(x)+".jpg")
+        except urllib.error.HTTPError as err:
+                print(err.code)
         url="https://s.4cdn.org/image/title/"+str(x)+".png"
         print(url)
-        urllib.request.urlretrieve(url,str(x)+".png")
-
+        try:
+                urllib.request.urlretrieve(url,str(x)+".png")
+        except urllib.error.HTTPError as err:
+                print(err.code)
 
