d39ed261088b3b7cc9566dc26572ac29f027cf16
Graeme Wolfendale <gwolfendale@gmail.com>
2014-07-02 20:29:40 +0100

    Update 4banner.py
    cross platform

--- 4banner.py
+++ 4banner.py
@@ -2,12 +2,13 @@ import urllib, os
 for x in range(0, 400):
         url="https://s.4cdn.org/image/title/"+str(x)+".gif"
         print url
-        os.system('wget %s' % url)
+        urllib.urlretrieve(url,str(x)+".gif")
         url="https://s.4cdn.org/image/title/"+str(x)+".jpg"
         print url
-        os.system('wget %s' % url)
+        urllib.urlretrieve(url,str(x)+".jpg")
         url="https://s.4cdn.org/image/title/"+str(x)+".png"
         print url
-        os.system('wget %s' % url)
+        urllib.urlretrieve(url,str(x)+".png")
+
 
 
