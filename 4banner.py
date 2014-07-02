1500effd135060dc5b877bc203ae92b00aa15b20
infoburp <gwolfendale@gmail.com>
2014-07-02 21:40:32 +0100

    much more DRY code

--- 4banner.py
+++ 4banner.py
@@ -1,21 +1,13 @@
 import urllib.request, urllib.parse, urllib.error, os
+extensions = {".gif",
+".jpg",
+".png"}
 for x in range(0, 400):
-	url="https://s.4cdn.org/image/title/"+str(x)+".gif"
-	try:
-		urllib.request.urlretrieve(url,str(x)+".gif")
-		print(url)
-	except urllib.error.HTTPError as err:
-		print(err.code)
-	url="https://s.4cdn.org/image/title/"+str(x)+".jpg"
-	try:
-		urllib.request.urlretrieve(url,str(x)+".jpg")
-		print(url)
-	except urllib.error.HTTPError as err:
-		print(err.code)
-	url="https://s.4cdn.org/image/title/"+str(x)+".png"
-	try:
-		urllib.request.urlretrieve(url,str(x)+".png")
-		print(url)
-	except urllib.error.HTTPError as err:
-		print(err.code)
+	for extension in extensions:
+		url="https://s.4cdn.org/image/title/"+str(x)+extension
+		try:
+			urllib.request.urlretrieve(url,str(x)+extension)
+			print(url)
+		except urllib.error.HTTPError as err:
+			print(err.code)
 
