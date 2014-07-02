fd55a15bf232000855c2b0b682f9ddc56ad1c417
infoburp <gwolfendale@gmail.com>
2014-07-02 21:20:55 +0100

    do not print URL for items that 404

--- 4banner.py
+++ 4banner.py
@@ -1,21 +1,21 @@
 import urllib.request, urllib.parse, urllib.error, os
 for x in range(0, 400):
-        url="https://s.4cdn.org/image/title/"+str(x)+".gif"
-        print(url)
-        try:
-                urllib.request.urlretrieve(url,str(x)+".gif")
-        except urllib.error.HTTPError as err:
-                print(err.code)
-        url="https://s.4cdn.org/image/title/"+str(x)+".jpg"
-        print(url)
-        try:
-                urllib.request.urlretrieve(url,str(x)+".jpg")
-        except urllib.error.HTTPError as err:
-                print(err.code)
-        url="https://s.4cdn.org/image/title/"+str(x)+".png"
-        print(url)
-        try:
-                urllib.request.urlretrieve(url,str(x)+".png")
-        except urllib.error.HTTPError as err:
-                print(err.code)
+	url="https://s.4cdn.org/image/title/"+str(x)+".gif"
+	try:
+		urllib.request.urlretrieve(url,str(x)+".gif")
+		print(url)
+	except urllib.error.HTTPError as err:
+		print(err.code)
+	url="https://s.4cdn.org/image/title/"+str(x)+".jpg"
+	try:
+		urllib.request.urlretrieve(url,str(x)+".jpg")
+		print(url)
+	except urllib.error.HTTPError as err:
+		print(err.code)
+	url="https://s.4cdn.org/image/title/"+str(x)+".png"
+	try:
+		urllib.request.urlretrieve(url,str(x)+".png")
+		print(url)
+	except urllib.error.HTTPError as err:
+		print(err.code)
 
