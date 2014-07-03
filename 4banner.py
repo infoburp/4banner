42b192b171aebcaafb201277a62de457f3340207
Graeme Wolfendale <gwolfendale@gmail.com>
2014-07-03 01:14:58 +0100

    Update 4banner.py

--- 4banner.py
+++ 4banner.py
@@ -2,11 +2,11 @@ import urllib.request, urllib.parse, urllib.error, os
 extensions = {".gif",
 ".jpg",
 ".png"}
-for x in range(0, 400):
+for filenumber in range(0, 400):
 	for extension in extensions:
-		url="https://s.4cdn.org/image/title/"+str(x)+extension
+		url="https://s.4cdn.org/image/title/"+str(filenumber)+extension
 		try:
-			urllib.request.urlretrieve(url,str(x)+extension)
+			urllib.request.urlretrieve(url,str(filenumber)+extension)
 			print(url)
 		except urllib.error.HTTPError as err:
 			print(err.code)
