9ab3fb323323ece7b14e254a6322fba003bcc255
Graeme Wolfendale <gwolfendale@gmail.com>
2014-07-03 12:16:13 +0100

    Update 4banner.py

--- 4banner.py
+++ 4banner.py
@@ -1,7 +1,5 @@
 import urllib.request, urllib.parse, urllib.error, os
-extensions = {".gif",
-".jpg",
-".png"}
+extensions = {".gif",".jpg",".png"}
 for filenumber in range(0, 400):
 	for extension in extensions:
 		url="https://s.4cdn.org/image/title/"+str(filenumber)+extension
